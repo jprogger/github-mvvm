ec33ee3672104b8bdf27dae31b09130e10afb2ea
Renat Ibragimov <jprogger@gmail.com>
2022-10-24 13:23:22 +0200

    added retrofit, serialization, okhttp deps

--- data/network/build.gradle.kts
+++ data/network/build.gradle.kts
@@ -7,6 +7,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:${ Versions.appCompatVersion }")
     implementation("com.google.android.material:material:${ Versions.materialVersion }")
 
+    // Kotlin Json Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${ Versions.kotlinSerialJsonVersion }")
+
+    //Retrofit2
+    implementation("com.squareup.retrofit2:retrofit:${Versions.retrofitVersion}")
+
+    //OkHttp (Retrofit dependency)
+    implementation("com.squareup.okhttp3:okhttp:${Versions.okhttpVersion}")
+
     testImplementation("junit:junit:${ Versions.junitVersion }")
     androidTestImplementation("androidx.test.ext:junit:${ Versions.testExtJunitVersion }")
     androidTestImplementation("androidx.test.espresso:espresso-core:${ Versions.espressoVersion }")
